1b7209ddb4fe2da572fa1cc86837de0e1a6340f2
Tino Reyna <tinoreyna1984@gmail.com>
2022-03-10 23:46:23 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
+import React, { useState, useEffect } from "react"; // React + hooks
 import logo from './logo.svg';
 import './App.css';
 
+/**
+ * Ejercicios para uso de hooks
+ */
+
 function App() {
+
+  // uso de useState
+  const [stateCar, setStateCar] = useState(false);
+
+  // uso de useEffect
+  const [contar, setContar] = useState(0)
+  /*useEffect(() => {
+    // lo que se ejecuta
+  }, [valores en array que deben actualizarse])*/
+  useEffect(() => {
+    console.log("clics: " + contar);
+  }, [contar]);
+  
+
+  // funcion del cambio de estado
+  const encenderApagar = () => {
+    setStateCar(!stateCar); // el cambio de estado
+    setContar(contar + 1);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          El coche está: {stateCar ? "Encendido" : "Apagado"}.<br/>
+          Número de clics: {contar} <br/>
+        <button onClick={() => encenderApagar()}>Encender/Apagar</button><br/>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
